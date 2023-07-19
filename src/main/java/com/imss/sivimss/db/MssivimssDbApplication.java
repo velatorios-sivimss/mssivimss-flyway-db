0466e928edc51cf012fd7e93db6a63daaa3cf133
Domingo Fernández <domingo.fernandez@people-media.com.mx>
2023-07-19 09:56:15 -0600

    se agrega proyacto base mssivimss-flyway-db

---
+++ src/main/java/com/imss/sivimss/db/MssivimssDbApplication.java
@@ -0,0 +1,13 @@
+package com.imss.sivimss.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssivimssDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MssivimssDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imss/sivimss/db/MssivimssDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imss.sivimss.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MssivimssDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
